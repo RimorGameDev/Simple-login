b5e1e188f45b8090d88752c4f639b6fcad64ce65
Rimor <59154276+RimorGameDev@users.noreply.github.com>
2020-06-09 14:14:22 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MidlleNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите логин: ");
+            string login = Console.ReadLine();
+            int att = 0;
+            while(login != "Nick")
+            {
+                att++;
+                Console.WriteLine("Неправильный логин!");
+                login = Console.ReadLine();
+
+                if(att >= 3)
+                {
+                    Console.WriteLine("Попытки кончились!");
+                    break;
+                }
+            }
+            Console.WriteLine("Пароль: ");
+            string pass = Console.ReadLine();
+            while (pass != "12345")
+            {
+                att++;
+                Console.WriteLine("Неправильный пароль");
+                pass = Console.ReadLine();
+                if (att >= 3)
+                {
+                    Console.WriteLine("Попытки кончились!");
+                    break;
+                }
+            }
+            Console.WriteLine("Успешно!");
+            
+        }
+    }
+}
